a95b0e922e70aaa5f680f7174c5a25f27b6a3acd
kimho <rlaghekd777@naver.com>
2021-12-03 20:57:58 +0900

    Changes

---
+++ letsStart/src/cats/cats.route.ts
@@ -0,0 +1,56 @@
+import { Cat } from "./app.model";
+
+app.get("/cats", (req: express.Request, res: express.Response) => {
+  try {
+    const cats = Cat;
+    res.status(200).send({
+      success: true,
+      data: {
+        cats,
+      },
+    });
+  } catch (error) {
+    res.status(400).send({
+      success: false,
+      error: error.message,
+    });
+  }
+});
+
+app.get("/cats/:id", (req: express.Request, res: express.Response) => {
+  try {
+    const params = req.params;
+    console.log(params);
+    const cat = Cat.find((cat) => {
+      return cat.id === params.id;
+    });
+    res.status(200).send({
+      success: true,
+      data: {
+        cat,
+      },
+    });
+  } catch (error) {
+    res.status(400).send({
+      success: false,
+      error: error.message,
+    });
+  }
+});
+
+app.post("/cats/", (req: express.Request, res: express.Response) => {
+  try {
+    const data = req.body;
+    console.log(data);
+    Cat.push(data);
+    res.status(200).send({
+      success: true,
+      data: { data },
+    });
+  } catch (error) {
+    res.status(400).send({
+      success: false,
+      error: error.message,
+    });
+  }
+});
