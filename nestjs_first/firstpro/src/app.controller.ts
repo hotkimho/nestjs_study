961d5a660e3de625d434db8a6b5789b0517ed811
kimho <rlaghekd777@naver.com>
2021-12-06 20:56:00 +0900

    test @decorator

--- nestjs_first/firstpro/src/app.controller.ts
+++ nestjs_first/firstpro/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Redirect } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,7 +6,8 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @Redirect('https://naver.com', 301)
   getHello(): string {
-    return this.appService.getHello();
+    return this.appService.getHello() + 'sdsd';
   }
 }
