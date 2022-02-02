7c89432a1973bcb3133d8dabf4e45322dcbf9dd3
hokim <rlaghekd777@naver.com>
2022-02-02 20:38:42 +0900

    add multer

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,12 +6,14 @@ import { LoggerMiddleware } from './common/middlewares/logger.middleware';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 import * as mongoose from 'mongoose';
-import {AuthModule} from "./auth/auth.module";
+import { AuthModule } from './auth/auth.module';
+import { CommentsModule } from './comments/comments.module';
 
 @Module({
   imports: [
     CatsModule,
     AuthModule,
+    CommentsModule,
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.MONGODBURI, {
       useNewUrlParser: true,
@@ -23,6 +25,7 @@ import {AuthModule} from "./auth/auth.module";
 })
 export class AppModule implements NestModule {
   private readonly isDev: boolean = process.env.MODE === 'dev';
+
   configure(consumer: MiddlewareConsumer) {
     consumer.apply(LoggerMiddleware).forRoutes('*');
     mongoose.set('debug', this.isDev);
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/exceptions/http-exception.filter';
-import {ValidationPipe} from "@nestjs/common";
-import {DocumentBuilder, OpenAPIObject, SwaggerModule} from "@nestjs/swagger";
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
+import * as expressBasicAuth from 'express-basic-auth';
+import * as path from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new HttpExceptionFilter());
+  app.use(
+    ['/docs', '/docs-json'],
+    expressBasicAuth({
+      challenge: true,
+      users: {
+        [process.env.SWAGGER_USER]: process.env.SWAGGER_PASSWORD,
+      },
+    }),
+  );
 
+  app.useStaticAssets(path.join(__dirname, './common', 'uploads'), {
+    prefix: '/media',
+  });
 
   const config = new DocumentBuilder()
     .setTitle('ho_nestjs')
@@ -21,7 +36,7 @@ async function bootstrap() {
   const PORT = process.env.PORT;
   app.enableCors({
     origin: true,
-    credentials: true
+    credentials: true,
   });
   await app.listen(PORT);
 }
